091f752caf666a3884eb60c5ebc351028c957575
IKUN-114 <130162337+IKUN-114@users.noreply.github.com>
2023-04-08 17:19:16 +0800

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -22,73 +22,73 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <style>
-        body {
-            background-image: url(images/clouds-sky.bmp);;
-            font-size: 0.8em;
-            margin: 0px;
-        }
+           body {
+              background-image: url(images/clouds-sky.bmp);;
+              font-size: 0.8em;
+              margin: 0px;
+          }
 
-        nav, section, article {
-            margin: 0px;
-            padding: 8px;
-        }
+          nav, section, article {
+              margin: 0px;
+              padding: 8px;
+          }
 
-        header, footer {
-            background-color: #328db5;
-            color: white;
-            margin: 0px;
-            padding: 5px;
-            padding-left:16px;
+          header, footer {
+              background-color: #328db5;
+              color: white;
+              margin: 0px;
+              padding: 5px;
+              padding-left:16px;
            
 
-        }
+          }
 
 
-        nav ul {
-            margin: 0;
-            padding: 0;
-        }
+          nav ul {
+              margin: 0;
+              padding: 0;
+          }
 
-            nav ul li {
+          nav ul li {
                 display: inline;
                 margin: 5px;
-            }
+          }
             
-        p {
-            color: grey;
-        }
+          p {
+              color: grey;
+          }
 
-        .bar{
-            margin: 0px;
-            padding: 0px;
-            width: 100%;
-            hieght: 32px;
-            background-color: rgba(250,250,250,0.4);
-            border-radius: 20px;
-        }
-    </style>
+          .bar{
+              margin: 0px;
+              padding: 0px;
+              width: 100%;
+              hieght: 32px;
+              background-color: rgba(250,250,250,0.4);
+              border-radius: 20px;
+          }
+        </style>
       </Head>
       <Body>
         
-    <header>
-        <h1>欢迎访问chei豆二连</h1>
-    </header>
+        <header>
+          <h1>欢迎访问chei豆二连</h1>
+        </header>
 
-    <nav>
-        <ul class="bar">
+        <nav>
+           <ul class="bar">
             <li><a href='#' style="color: #328db5">主页</a></li>
             <li><a href='#' style="color: lightblue">成员</a></li>
             <li><a href='#' style="color: lightblue; float: right; padding-right: 16px">消息</a></li>
-        </ul>
-    </nav>
+            </ul>
+        </nav>
 
-    <section>
+        <section>
 
-        <h1 style="color: steelblue">chei豆二连</h1>
+           <h1 style="color: steelblue">chei豆二连</h1>
 
-        <article>
-            <h2 style="color: lightblue">公告栏</h2>
-            <p style="color: grey">
+           <article>
+              <h2 style="color: lightblue">公告栏</h2>
+              <p style="color: grey">
                 ·建立时间：公元 2022 年 9 月 1 日（永生不灭）
                 <br/>
                 <br/>
@@ -99,13 +99,13 @@ export default function Root() {
                 <br/>
                 <br/>
                 ·帝都：生京
-            </p>
-          </article>
+              </p>
+           </article>
 
-          </section>
+           </section>
 
-          <!-- <footer>
-          <p>&copy; 2020 App Name . All rights reserved.</p> 
+           <!-- <footer>
+           <p>&copy; 2023 chei豆二连 . All rights reserved.</p> 
         </footer> -->
       </Body>
     </Html>
