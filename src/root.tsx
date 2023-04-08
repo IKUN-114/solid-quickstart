7f17d42976aa3ca12777d1ae539d00e3f8f0c824
IKUN-114 <130162337+IKUN-114@users.noreply.github.com>
2023-04-08 17:11:22 +0800

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -18,20 +18,24 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>这里是 chei 豆二连</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
+        · 建立时间：公元2022年9月一日
+        <br/>
+        <br/>
+        · 建立者：chei
+        <br/>
+        <br/>
+        · 国号：chei
+        <br/>
+        <br/>
+        · 帝都：生京
+        <br/>
+        <br/>
+        <a href="http://www.baidu.com">了解更多 &raquo;</a>
         <Scripts />
       </Body>
     </Html>
