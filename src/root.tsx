080c57fd5fae19c1a7c380b3e6a27fcb07adb788
IKUN-114 <130162337+IKUN-114@users.noreply.github.com>
2023-04-08 17:14:54 +0800

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -21,22 +21,92 @@ export default function Root() {
         <Title>这里是 chei 豆二连</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style>
+        body {
+            background-image: url(images/clouds-sky.bmp);;
+            font-size: 0.8em;
+            margin: 0px;
+        }
+
+        nav, section, article {
+            margin: 0px;
+            padding: 8px;
+        }
+
+        header, footer {
+            background-color: #328db5;
+            color: white;
+            margin: 0px;
+            padding: 5px;
+            padding-left:16px;
+           
+
+        }
+
+
+        nav ul {
+            margin: 0;
+            padding: 0;
+        }
+
+            nav ul li {
+                display: inline;
+                margin: 5px;
+            }
+            
+        p {
+            color: grey;
+        }
+
+        .bar{
+            margin: 0px;
+            padding: 0px;
+            width: 100%;
+            hieght: 32px;
+            background-color: rgba(250,250,250,0.4);
+            border-radius: 20px;
+        }
+    </style>
       </Head>
       <Body>
-        · 建立时间：公元2022年9月一日
-        <br/>
-        <br/>
-        · 建立者：chei
-        <br/>
-        <br/>
-        · 国号：chei
-        <br/>
-        <br/>
-        · 帝都：生京
-        <br/>
-        <br/>
-        <a href="http://www.baidu.com">了解更多 &raquo;</a>
-        <Scripts />
+        
+    <header>
+        <h1>欢迎访问chei豆二连</h1>
+    </header>
+
+    <nav>
+        <ul class="bar">
+            <li><a href='#' style="color: #328db5">主页</a></li>
+            <li><a href='#' style="color: lightblue">成员</a></li>
+            <li><a href='#' style="color: lightblue; float: right; padding-right: 16px">消息</a></li>
+        </ul>
+    </nav>
+
+    <section>
+
+        <h1 style="color: steelblue">chei豆二连</h1>
+
+        <article>
+            <h2 style="color: lightblue">公告栏</h2>
+            <p style="color: grey">
+                ·建立时间：公元 2022 年 9 月 1 日（永生不灭）
+                <br/>
+                <br/>
+                ·建立者：shie
+                <br/>
+                <br/>
+                ·国号：chei
+                <br/>
+                <br/>
+                ·帝都：生京
+            </p>
+          </article>
+
+          </section>
+
+          <!-- <footer>
+          <p>&copy; 2020 App Name . All rights reserved.</p> 
+        </footer> -->
       </Body>
     </Html>
   );
