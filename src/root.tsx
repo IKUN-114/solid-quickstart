055965f9478e5fdc48f59408ea97799d207555b3
IKUN-114 <130162337+IKUN-114@users.noreply.github.com>
2023-04-08 17:38:41 +0800

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -26,69 +26,68 @@
 
         }
 
+      nav ul {
+            margin: 0;
+            padding: 0;
+        }
 
-          nav ul {
-              margin: 0;
-              padding: 0;
-          }
-
-          nav ul li {
-                display: inline;
-                margin: 5px;
-          }
+      nav ul li {
+          display: inline;
+          margin: 5px;
+      }
             
-          p {
-              color: grey;
-          }
+      p {
+          color: grey;
+      }
 
-          .bar{
-              margin: 0px;
-              padding: 0px;
-              width: 100%;
-              hieght: 32px;
-              background-color: rgba(250,250,250,0.4);
-              border-radius: 20px;
-          }
-        </style>
-      </Head>
-      <Body>
+      .bar{
+          margin: 0px;
+          padding: 0px;
+          width: 100%;
+          hieght: 32px;
+          background-color: rgba(250,250,250,0.4);
+          border-radius: 20px;
+      }
+    </style>
+  </Head>
+  <Body>
         
-        <header>
-          <h1>欢迎访问chei豆二连</h1>
-        </header>
+    <header>
+      <h1>欢迎访问chei豆二连</h1>
+    </header>
 
-        <nav>
-           <ul class="bar">
-            <li><a href='#' style="color: #328db5">主页</a></li>
-            <li><a href='#' style="color: lightblue">成员</a></li>
-            <li><a href='#' style="color: lightblue; float: right; padding-right: 16px">消息</a></li>
-            </ul>
-        </nav>
+    <nav>
+      <ul class="bar">
+        <li><a href='#' style="color: #328db5">主页</a></li>
+        <li><a href='#' style="color: lightblue">成员</a></li>
+        <li><a href='#' style="color: lightblue; float: right; padding-right: 16px">消息</a></li>
+      </ul>
+      </nav>
 
-        <section>
+      <section>
 
-          <h1 style="color: steelblue">chei豆二连</h1>
+        <h1 style="color: steelblue">chei豆二连</h1>
   
-          <article>
-            <h2 style="color: lightblue">公告栏</h2>
-            <p style="color: grey">
-              ·建立时间：公元 2022 年 9 月 1 日（永生不灭）
-              <br/>
-              <br/>
-              ·建立者：shie
-              <br/>
-              <br/>
-              ·国号：chei
-              <br/>
-              <br/>
-              ·帝都：生京
-            </p>
-         </article>
+        <article>
+          <h2 style="color: lightblue">公告栏</h2>
+          <p style="color: grey">
+            ·建立时间：公元 2022 年 9 月 1 日（永生不灭）
+            <br/>
+            <br/>
+            ·建立者：shie
+            <br/>
+            <br/>
+            ·国号：chei
+            <br/>
+            <br/>
+            ·帝都：生京
+          </p>
+       </article>
 
-         </section>
+       </section>
 
-         <!-- <footer>
-         <p>&copy; 2023 chei豆二连 . All rights reserved.</p> 
-      </footer> -->
-    </Body>
+       <!-- <footer>
+       <p>&copy; 2023 chei豆二连 . All rights reserved.</p> 
+    </footer> -->
+</Body>
 </html>
