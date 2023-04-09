1428817efb1509ef764c1bc44d1a1a00b8e4971e
IKUN-114 <130162337+IKUN-114@users.noreply.github.com>
2023-04-09 21:34:42 +0800

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,15 +4,15 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>未找到该页面</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>404 <small>页面不存在<small></h1>
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
-        to learn how to build SolidStart apps.
+        对不起，未发现您要找的内容
       </p>
     </main>
   );
