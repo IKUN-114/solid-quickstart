11d1f15fac263a74c9c1418e84a4bc8558d01a0d
IKUN-114 <130162337+IKUN-114@users.noreply.github.com>
2023-04-10 21:44:10 +0800

    Update root.tsx

--- 来源/root.tsx
+++ 来源/root.tsx
@@ -4,16 +4,30 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Chei 豆二连</Title>
+      <h1>欢迎访问 chei 豆二连</h1>
       <Counter />
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+        <small>点击此处为 chei 豆二连助力吧！
+        <br/>
+        <br/>
+        ·建立时间：公元 2022 年 9 月 1 日（永生不灭）
+                <br/>
+                <br/>
+                ·建立者：shie
+                <br/>
+                <br/>
+                ·国号：chei
+                <br/>
+                <br/>
+                ·帝都：生京
+        <br/>
+        <a href="http://www.python.org" style="float: right">了解更多 &raquo;</a>    
+       </p>
     </main>
   );
 }
